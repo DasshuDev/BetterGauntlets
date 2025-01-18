786692e03dab309774a9c0f1913de59887d084cd
Capeling <113705504+Capeling@users.noreply.github.com>
2025-01-18 17:28:33 +1030

    new island

---
+++ src/GauntletNode.cpp
@@ -0,0 +1,12 @@
+#include "GauntletNode.hpp"
+
+using namespace geode::prelude;
+
+gd::string RedesignedGauntletNode::frameForType(GauntletType type) {
+    switch(type) {
+        case GauntletType::Fire: return "island_lvl_fire_001.png"_spr;
+        case GauntletType::Ice: return "island_lvl_ice_001.png"_spr;
+        case GauntletType::Poison: return "island_lvl_poison_001.png"_spr;
+        default: return GauntletNode::frameForType(type);
+    }
+}

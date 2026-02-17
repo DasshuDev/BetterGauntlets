0f3bc56159ec63da21ae4deade17d88f3857b83c
Jay <racerjay03@gmail.com>
2026-02-17 18:54:19 -0500

    okay 1.7.0 for real this time

--- src/GauntletLayer/GauntletEdits/57_love.cpp
+++ src/GauntletLayer/GauntletEdits/57_love.cpp
@@ -12,19 +12,19 @@ void RedesignedGauntletLayer::editLoveGauntlet() {
     
     m_backgroundSprite->setTexture(CCTextureCache::sharedTextureCache()->textureForKey(GameManager::get()->getBGTexture(backgroundValue)));
 
-    auto enableParticles = Mod::get()->getSettingValue<bool>("enable-background-particles");
-	if (enableParticles) {
-        // hearts
-		auto bgParticleNode1 = CCParticleSystemQuad::create();
-        if (bgParticleNode1) {
-            CCParticleSystemQuad* bgParticles = GameToolbox::particleFromString(
-                "70a-1a2a0.45a28a90a20a0a0a300a200a0a0a0a0a0a0a5a2a0a43a1a0a1a0a1a0a1a0a10a3a0a38a1a0a0.501961a0a0a0a0.5a0.15a0a0a0.45a0a0a0a0a0a0a0a0a2a1a0a0a0a169a0a0a0a0a0a0a0a0a0a0a0a0a0a0",
-                NULL,
-                false
-            );
-            bgParticles->setPosition(ccp(winSize.width / 2, winSize.height / 2));
-            bgParticles->setZOrder(-98);
-            this->addChild(bgParticles);
-        }
-    }
+    // auto enableParticles = Mod::get()->getSettingValue<bool>("enable-background-particles");
+	// if (enableParticles) {
+    //     // hearts
+	// 	auto bgParticleNode1 = CCParticleSystemQuad::create();
+    //     if (bgParticleNode1) {
+    //         CCParticleSystemQuad* bgParticles = GameToolbox::particleFromString(
+    //             "70a-1a2a0.45a28a90a20a0a0a300a200a0a0a0a0a0a0a5a2a0a43a1a0a1a0a1a0a1a0a10a3a0a38a1a0a0.501961a0a0a0a0.5a0.15a0a0a0.45a0a0a0a0a0a0a0a0a2a1a0a0a0a169a0a0a0a0a0a0a0a0a0a0a0a0a0a0",
+    //             NULL,
+    //             false
+    //         );
+    //         bgParticles->setPosition(ccp(winSize.width / 2, winSize.height / 2));
+    //         bgParticles->setZOrder(-98);
+    //         this->addChild(bgParticles);
+    //     }
+    // }
 }

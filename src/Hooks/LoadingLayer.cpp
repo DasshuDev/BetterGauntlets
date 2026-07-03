251cbd858a40b1caf63f0a61226d744163ef902d
Jay <racerjay03@gmail.com>
2026-07-03 16:29:11 -0400

    more to-do

--- src/Hooks/LoadingLayer.cpp
+++ src/Hooks/LoadingLayer.cpp
@@ -8,14 +8,11 @@ class $modify(GRLoadingLayer, LoadingLayer) {
     bool init(bool refresh) {
         if (!LoadingLayer::init(refresh)) return false;
 
-        // Warm the manager cache as early as possible with the full manager
-        // list, so every profile/comment badge check anywhere resolves
-        // instantly instead of needing its own network round-trip.
         GauntletManagerCache::get()->warm();
 
         auto accountID = GJAccountManager::get()->m_accountID;
         GauntletManagerCache::get()->isManager(accountID, [accountID](bool isManager) {
-            log::info("[GauntletRedesign] Account {} is {}a Gauntlet Manager", accountID, isManager ? "" : "not ");
+            log::info("Account {} is {}a Gauntlet Manager", accountID, isManager ? "" : "not ");
         });
 
         return true;
